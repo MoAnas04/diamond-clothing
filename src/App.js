765f3620ae2ad17edf70f971d315728c72bce122
MoAnas04 <anas.mansuri.90281@gmail.com>
2021-08-17 20:32:20 +0530

    shop page styling done and added to our app using collection-preview component and collection-item component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 function App() {
   return (
     <div>
       <Switch>
         <Route exact component={HomePage} path="/" />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
         {/* exact takes true or false and switch make sure that our browser displays only route which matches and nothing else*/}
       </Switch>
     </div>
