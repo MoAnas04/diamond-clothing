a3afd07e7614d9e3048a2c49e96cf8d98b5ee09a
MoAnas04 <anas.mansuri.90281@gmail.com>
2021-08-17 19:05:13 +0530

    upadte our menu-iems to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact component={HomePage} path="/" />
+        <Route path="/shop/hats" component={HatsPage} />
+        {/* exact takes true or false and switch make sure that our browser displays only route which matches and nothing else*/}
+      </Switch>
     </div>
   );
 }
